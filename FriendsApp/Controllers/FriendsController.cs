544e90189bf7f2d84cb73b2c7fd5e9c805cf141b
AliDM <dahmardeh.ali@gmail.com>
2022-05-18 10:25:29 +0430

    Add project files.

---
+++ FriendsApp/Controllers/FriendsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FriendsApp.Controllers
+{
+    public class FriendsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
