d1f895c7a21e0b8417e78f2061fd44e46acd75f2
AliDM <dahmardeh.ali@gmail.com>
2022-05-18 11:17:12 +0430

    EntityFramework

---
+++ FriendsApp/Data/Context/FriendsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FriendsApp.Data.Context
+{
+    public class FriendsDbContext : DbContext
+    {
+        public FriendsDbContext(DbContextOptions<FriendsDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        DbSet<Friend> Friends { get; set; }
+    }
+}
---
+++ FriendsApp/Data/Friend.cs
@@ -0,0 +1,10 @@
+﻿namespace FriendsApp.Data
+{
+    public class Friend
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int FriendId { get; set; }
+        public string Image { get; set; }
+    }
+}
